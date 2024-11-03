4aca39cfc7656e769c367c4c27858103d121e1be
unknown <nicolasgonzalezmoller@gmail.com>
2024-11-03 20:09:32 -0300

    metadata

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,18 +2,78 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "WebZait",
+  title: "WebZait | Páginas y aplicaciones Web ",
   description: "Sitios Web de alto rendimiento",
+  keywords: [
+    "desarrollo web",
+    "diseño de páginas web",
+    "creación de ecommerces",
+    "aplicaciones web",
+    "páginas web personalizadas",
+    "sitios web de alto rendimiento",
+    "tiendas online",
+    "landing pages",
+    "web corporativa",
+    "diseño de tiendas online",
+    "soluciones digitales",
+    "sitios responsivos",
+    "optimización web",
+    "desarrollo de aplicaciones",
+    "web profesional"
+  ]
+};
+
+const jsonLdData = {
+  "@context": "https://schema.org",
+  "@type": "LocalBusiness",
+  "name": "WebZait",
+  "url": "https://webzait.cl",
+  "logo": "./favicon.ico",
+  "description": "Sitios Web de alto rendimiento, tiendas online y aplicaciones web personalizadas.",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "Calle Ejemplo 123",
+    "addressLocality": "Santiago",
+    "addressRegion": "Región Metropolitana",
+    "postalCode": "8910090",
+    "addressCountry": "CL"
+  },
+  "contactPoint": {
+    "@type": "ContactPoint",
+    "telephone": "+56-981512691",
+    "contactType": "Customer Service"
+  }
+  // "sameAs": [
+  //   "https://www.facebook.com/WebZait",
+  //   "https://www.twitter.com/WebZait",
+  //   "https://www.instagram.com/WebZait"
+  // ]
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className={inter.className}><Navbar />{children}<Footer /></body>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords.join(", ")} />
+        <link rel="canonical" href="https://webzait.cl" />
+        {/* Script JSON-LD */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLdData) }}
+        />
+      </Head>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,29 @@
-import React from 'react'
+import React from 'react';
 
 const Hero = () => {
     return (
-        <div
+        <section
             className="hero min-h-screen"
             style={{
                 backgroundImage: `url(https://res.cloudinary.com/dpbpyzl96/image/upload/v1724809911/iWEB/hpf22m3h6kwnl7atadl0.jpg)`,
-            }}>
+            }}
+            aria-label="Hero section for WebZait, specializing in high-performance web design and development."
+        >
             <div className="hero-overlay bg-opacity-80"></div>
             <div className="hero-content text-neutral-content text-center">
                 <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold text-white">WebZait</h1>
+                    <h1 className="mb-5 text-5xl font-bold text-white" aria-label="WebZait - sitios web de alto rendimiento">WebZait</h1>
 
-                    <p className="mb-5 text-3xl text-white">
+                    <p className="mb-5 text-3xl text-white" aria-label="paginas web personzalizadas para cada necesidad">
                         Sitios web de alto rendimiento, totalmente personalizados para tu negocio. Diseñamos y desarrollamos soluciones digitales únicas, optimizadas para ofrecer la mejor experiencia a tus usuarios y llevar tu proyecto al siguiente nivel.
                     </p>
 
+                    {/* Botón comentado para activarse en el futuro si es necesario */}
                     {/* <button className="btn btn-primary">Get Started</button> */}
                 </div>
             </div>
-        </div>
-    )
-}
+        </section>
+    );
+};
 
-export default Hero
+export default Hero;
